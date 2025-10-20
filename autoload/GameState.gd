b354f4bde2ec8dd3b9d4f548d1102973307d3bfa
Patrick <wi23b079@technikum-wien.at>
2025-10-20 13:36:48 +0200

    player selection added, female player playable

---
+++ autoload/GameState.gd
@@ -0,0 +1,12 @@
+extends Node
+
+enum Gender { MALE, FEMALE }
+var selected_gender: Gender = Gender.MALE
+
+const PLAYER_SCENES := {
+	Gender.MALE: preload("res://scenes/player_male.tscn"),
+	Gender.FEMALE: preload("res://scenes/player_female.tscn"),
+}
+
+func get_selected_player_scene() -> PackedScene:
+	return PLAYER_SCENES[selected_gender]
